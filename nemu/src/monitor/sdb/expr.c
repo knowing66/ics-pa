1854863258ffdd6626af8c7443e50a149aea4c63
tracer-ics2023 <tracer@njuics.org>
2024-03-31 19:36:05 +0800

    >  compile NEMU 23021211643 Shi Yang Linux shiyang-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:05 up 50 min,  1 user,  load average: 1.02, 0.68, 0.77

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -174,9 +174,9 @@ bool check_parentheses(Token *tokens,int leftpositon,int rightposition){
 }
 
 word_t evaluate(Token *tokens,int leftpositon,int rightposition){
-  //if(leftpositon>rightposition){
-   // return 0;
-  //}
+  if(leftpositon>rightposition){
+    assert(0);
+  }
   if(leftpositon==rightposition){
     word_t sum=0;
     word_t strpos=0;
